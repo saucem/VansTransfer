eb87e176a1c8daba21197a12a0f7cb45bfb9b476
Mauro Saucedo <saucem.dev@gmail.com>
2025-10-07 12:42:17 -0300

    New files created & JS modification

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,9 @@
+window.onload = function() {
+    // Reset the form fields when the page loads
+    document.getElementById("first-form").reset();
+    document.getElementById("second-form").reset();
+};
+
 const buttonToggler = document.querySelector(".navbar-toggler");
 const togglerIcon = document.getElementById("toggler-icon")
 
