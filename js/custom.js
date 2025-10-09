bd712e12780f4383ec2b7e5a9ce8b02399792dc7
saucem <saucem@gmail.com>
2025-10-09 04:12:03 -0300

    New images, improved styles, links setting & sizing corrections

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,6 @@
 window.onload = function() {
     // Reset the form fields when the page loads
     document.getElementById("first-form").reset();
-    document.getElementById("second-form").reset();
 };
 
 const buttonToggler = document.querySelector(".navbar-toggler");
@@ -13,3 +12,11 @@ buttonToggler.addEventListener("click", function() {
   togglerIcon.classList.toggle("bi-list");
   togglerIcon.classList.toggle("bi-x-lg");
 });
+
+function goBack() {
+  if (window.history.length > 1) {
+      window.history.back(); // Regresa a la página anterior
+  } else {
+      alert("No hay una página anterior en el historial.");
+  }
+}
